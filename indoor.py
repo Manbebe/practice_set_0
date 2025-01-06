7ed937e77a9daeef56a9c433c5536e1241c21ea8
Gregory McCormack <gregorym2802@tuckahoeschools.org>
2025-01-06 13:32:10 +0000

    init commit

---
+++ indoor.py
@@ -0,0 +1,10 @@
+def main():
+    yell = input("SCREAM YOUR HEART OUT!!!! ").strip().casefold()
+    lower(yell)
+
+
+def lower(quiet_yell):
+    print(f"dude why the flip are u yelling bro use your indoor voice, holy bum, say it quiet like {quiet_yell}")
+
+
+main()
